6aece037c996d3f1f09da131317f0575c1ed0235
Andrija Petrovic <andrija.hers@gmail.com>
2017-03-23 15:17:10 +0000

    Optimization of the `notify` mechanism

--- deferredcreator.js
+++ deferredcreator.js
@@ -2,10 +2,15 @@ function createDeferred(runNext, promises) {
   'use strict';
   function Deferred() {
     this.promise = new promises.Promise(this.onResolved.bind(this));
+    this.notificationvalues = [];
   }
   Deferred.prototype.resolve = function (value) {
     var p;
     if (this.promise instanceof promises.Promise) {
+      if (this.notificationvalues.length>0) {
+        runNext(this.resolve.bind(this, value));
+        return;
+      }
       p = this.promise.resolveForChain(value);
       if (p) {
         this.promise = p;
@@ -14,15 +19,27 @@ function createDeferred(runNext, promises) {
   };
   Deferred.prototype.reject = function (value) {
     if (this.promise instanceof promises.Promise) {
+      if (this.notificationvalues.length>0) {
+        runNext(this.reject.bind(this, value));
+        return;
+      }
       this.promise.reject(value);
       this.promise = new promises.RejectedPromise(value);
     }
   };
   Deferred.prototype.notify = function (value) {
     if (this.promise instanceof promises.Promise) {
-      runNext(this.promise.notify.bind(this.promise, value));
+      this.notificationvalues.push(value);
+      if (this.notificationvalues.length===1) {
+        runNext(this.fireNotifications.bind(this));
+      }
       value = null;
     }
+  };
+  Deferred.prototype.fireNotifications = function () {
+    var nvs = this.notificationvalues;
+    this.notificationvalues = [];
+    nvs.forEach(this.promise.notify.bind(this.promise));
   };
   Deferred.prototype.onResolved = function (promise) {
     this.promise = promise;
